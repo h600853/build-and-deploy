d9995a686f311e1d6beb57beed63d176c3f2fb60
Sondre <89258011+h600853@users.noreply.github.com>
2023-10-31 19:11:04 +0100

    get minutes left of year

--- code/src/clock.test.js
+++ code/src/clock.test.js
@@ -1,4 +1,4 @@
-import { getSecondsLeftOfYear, getTimeString } from "./clock";
+import { getSecondsLeftOfYear, getTimeString, getMinutesLeftOfYear} from "./clock";
 
 test('getTimeString formats time string correctly', () => {
     const now = new Date(0, 0, 0, 10, 20, 30);
@@ -10,5 +10,10 @@ test('getSecondsLeftOfYear returns the correct amount of seconds', () => {
     const now = new Date("2022-12-31T23:00:00.000Z");
     const timeString = getSecondsLeftOfYear(now);
     expect(timeString).toBe(3600);
+});
+test('getMinutesLeftOfYear returns the correct amount of minutes', () => {
+    const now = new Date("2022-12-31T23:00:00.000Z");
+    const timeString = getMinutesLeftOfYear(now);
+    expect(timeString).toBe(60);
 });
 
--- code/src/main.js
+++ code/src/main.js
@@ -1,12 +1,14 @@
-import {  getSecondsLeftOfYear, getTimeString } from "./clock";
+import {  getSecondsLeftOfYear, getTimeString, getMinutesLeftOfYear } from "./clock";
 
 const timeContainer = document.getElementById("time");
 const timeLeft = document.getElementById("seconds-left");
+const timeLeftMinutes = document.getElementById("minutes-left");
 
 const render = () => {
   const now = new Date();
   timeLeft.innerText = getSecondsLeftOfYear(now);
   timeContainer.innerText = getTimeString(now);
+  timeLeftMinutes.innerText = getMinutesLeftOfYear(now);
 }
 
 render();
