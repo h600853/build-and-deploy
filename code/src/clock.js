a37abe4e4f4b1c1a51b1578da2e27385a439cfd8
Sondre <89258011+h600853@users.noreply.github.com>
2023-10-31 19:25:44 +0100

    format clock.

--- code/src/clock.js
+++ code/src/clock.js
@@ -1,9 +1,10 @@
 export const getTimeString = (now) => {
-    const hour = now.getHours().padStart(2, '0');
-    const minute = now.getMinutes().padStart(2, '0');
-    const seconds = now.getSeconds().padStart(2, '0');
-    return `${hour}:${minute}:${seconds}`
+    const hour = String(now.getHours()).padStart(2, '0');
+    const minute = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    return `${hour}:${minute}:${seconds}`;
 }
+
 
 const getNextYearDate = (now) => {
     const endOfYearDate = new Date(0)
--- code/src/clock.test.js
+++ code/src/clock.test.js
@@ -1,16 +1,16 @@
 import { getSecondsLeftOfYear, getTimeString, getMinutesLeftOfYear} from "./clock";
 
-test('getTimeString formats time string correctly', () => {
+test('getTimeString formats time string correctly for 10:20:30', () => {
     const now = new Date(0, 0, 0, 10, 20, 30);
     const timeString = getTimeString(now);
     expect(timeString).toBe('10:20:30');
 });
-test('getTimeString formats time string correctly', () => {
+
+test('getTimeString formats time string correctly for 01:01:01', () => {
     const now = new Date(0, 0, 0, 1, 1, 1);
     const timeString = getTimeString(now);
     expect(timeString).toBe('01:01:01');
 });
-
 test('getSecondsLeftOfYear returns the correct amount of seconds', () => {
     const now = new Date("2022-12-31T23:00:00.000Z");
     const timeString = getSecondsLeftOfYear(now);
